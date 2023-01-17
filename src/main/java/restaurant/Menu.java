fc2986d2b03f9827938063f79fbd904d19484e87
Tuma Ussiri <tussiri@gmail.com>
2023-01-17 10:49:23 -0600

    Studio completed

---
+++ src/main/java/restaurant/Menu.java
@@ -0,0 +1,14 @@
+package restaurant;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+
+    //TODO: Declare two private fields, menuItems (ArrayList<MenuItems>) and lastUpdated(Date)
+
+    private ArrayList<MenuItem> menuItems=new ArrayList <>();
+    private Date lastUpdated;
+
+}
---
+++ src/main/java/restaurant/MenuItem.java
@@ -0,0 +1,23 @@
+package restaurant;
+
+import java.util.Date;
+
+public class MenuItem {
+
+    //TODO: Declare private fields name, description, price, category, and dateAdded (Date)
+    private String name;
+    private String description;
+    private Double price;
+    private String category;
+    private Date dateAdded;
+
+    //TODO: Define constructor to take values for first four fields as params
+    //Initialize dateAdded to the current date
+    public MenuItem(String name, String description, double price, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.dateAdded = new Date();
+    }
+}
